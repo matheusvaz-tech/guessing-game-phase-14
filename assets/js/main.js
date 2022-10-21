c42a5c33a618212fc49f6d4a534cdd8087674876
Matheus Vaz <112774634+matheusvaz-dev@users.noreply.github.com>
2022-10-21 01:09:54 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,25 @@
+// Variáveis
 const screen_1 = document.querySelector("main");
 const screen_2 = document.querySelector("section");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
 
-const randomNumber = Number(Math.round(Math.random() * 10));
+let randomNumber = Number(Math.round(Math.random() * 10));
 let xAttempts = 1;
 
-// Função callback
+// Eventos
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener("keydown", pressEnter);
+
+//Funções
 function handleTryClick(event) {
   event.preventDefault(); // Não faça o padrão
 
   const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) === randomNumber) {
-    screen_1.classList.add("hide");
-    screen_2.classList.remove("hide");
-
+    toggleScreen();
     screen_2.querySelector("h2 strong").innerText = xAttempts; // Posso pegar um elemento dentro de outro que já foi pego (e declarado)
   }
 
@@ -22,14 +28,20 @@ function handleTryClick(event) {
   xAttempts++;
 }
 
-// Eventos
-const btnTry = document.querySelector("#btnTry");
-const btnReset = document.querySelector("#btnReset");
-
-btnTry.addEventListener("click", handleTryClick);
-btnReset.addEventListener("click", function () {
-  screen_1.classList.remove("hide");
-  screen_2.classList.add("hide");
-
+function handleResetClick() {
+  toggleScreen();
   xAttempts = 1;
-});
+
+  randomNumber = Number(Math.round(Math.random() * 10));
+}
+
+function toggleScreen() {
+  screen_1.classList.toggle("hide");
+  screen_2.classList.toggle("hide");
+}
+
+function pressEnter(e) {
+  if (e.key == "Enter" && screen_1.classList.contains("hide")) {
+    handleResetClick();
+  }
+}
